2b3d267a48d96de844d447e48a4bec00a5ac736d
jeff-hamm <jeff.hamm@gmail.com>
2015-03-25 08:09:56 +0000

    moreso

--- scripts/spreeini.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. /opt/bitnami/scripts/setenv.sh
-cd /opt/bitnami/apps/spree/htdocs
-bin/rake railties:install:migrations RAILS_ENV="production"
-bin/rake db:migrate RAILS_ENV="production"
-bin/rake db:seed RAILS_ENV="production" AUTO_ACCEPT="yes"
-bin/rake spree_sample:load RAILS_ENV="production" 
